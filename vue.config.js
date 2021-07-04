e00ba2af0c5b51c0fdcde710498f17991568a482
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2021-07-04 20:27:36 +0200

    Configurations for custom domain
    Fix 404 redirect

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/dannil.se' : '/',
+  // Since we're using a custom domain, we don't really need this check as Vue will be running
+  // at the root of all URLs.
+  // publicPath: process.env.NODE_ENV === 'production' ? '/dannil.se' : '/',
+  publicPath: '/',
   css: {
     loaderOptions: {
       sass: {
