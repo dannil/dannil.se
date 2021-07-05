fb3d8a55558ddb1eac2a4c2deb0cd3117b89804d
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2021-07-05 18:46:11 +0200

    First iteration of adding Google Analytics

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
+let isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   // Since we're using a custom domain, we don't really need this check as Vue will be running
-  // at the root of all URLs.
+  // at the root both in local dev and GitHub Pages.
   // publicPath: process.env.NODE_ENV === 'production' ? '/dannil.se' : '/',
   publicPath: '/',
   css: {
@@ -9,5 +10,14 @@ module.exports = {
         prependData: `@import "@/assets/styles/_variables.scss";`
       }
     }
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].isProduction = isProduction;
+        args[0].gaTag = process.env.VUE_APP_GA_TAG;
+        return args;
+      })
   }
 };
